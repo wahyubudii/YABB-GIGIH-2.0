d0c74123d7f7cc0892418b9b3799dc0d30085346
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-03-18 18:58:01 +0700

    update hands-on

--- LS 3/hands-on/src/App.js
+++ LS 3/hands-on/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hallo Generasi GIGIH 2.0</p>
+        <p>This is LS 3 Hands-on</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
