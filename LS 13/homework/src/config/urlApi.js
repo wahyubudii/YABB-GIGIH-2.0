341a536b205430bfe235ee64b5e7928a7be0f396
Wahyu Budi Utomo <79882296+wahyubudii@users.noreply.github.com>
2022-04-13 18:10:19 +0700

    add: LS 13 pure css Homepage

---
+++ LS 13/homework/src/config/urlApi.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "https://api.spotify.com/v1"
+
+export const USER = "/users"
+export const PLAYLIST = "/playlists"
+export const TRACKS = "/tracks"
+
+export const SEARCH = BASE_URL + "/search"
+export const CURRENT_USER = BASE_URL + "/me"
